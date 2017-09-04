038bf19b76ac0160ec07f45fb4f3d9a09f103f90
Umidbek Karimov <uma.karimov@gmail.com>
2017-09-04 20:58:09 +0400

    feat(docs): use `HashRouter` instead of `BrowserRouter`.

--- src/docs/docs.js
+++ src/docs/docs.js
@@ -1,15 +1,15 @@
 import "bootstrap/dist/css/bootstrap.css";
 import React from "react";
 import ReactDOM from "react-dom";
-import { BrowserRouter } from "react-router-dom";
+import HashRouter from "react-router-dom/HashRouter";
 
 import App from "./components/app/App";
 
 import { context } from "./pages";
 
 ReactDOM.render(
-  <BrowserRouter basename="/react-google-map-components">
+  <HashRouter>
     <App context={context} />
-  </BrowserRouter>,
+  </HashRouter>,
   document.getElementById("root"),
 );
