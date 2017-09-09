559de3855823294949f76e50ab0010a3e0ebcae8
Umidbek Karimov <uma.karimov@gmail.com>
2017-09-09 16:55:53 +0400

    refactor(docs): use `src/index.js` as a docs entry.

--- src/index.js
+++ src/index.js
@@ -1,1 +1,17 @@
-import "./docs/docs";
+// eslint-disable-next-line import/no-extraneous-dependencies
+import "bootstrap/dist/css/bootstrap.css";
+import React from "react";
+import ReactDOM from "react-dom";
+// eslint-disable-next-line import/no-extraneous-dependencies
+import HashRouter from "react-router-dom/HashRouter";
+
+import App from "./docs/components/app/App";
+
+import { context } from "./docs/pages";
+
+ReactDOM.render(
+  <HashRouter>
+    <App context={context} />
+  </HashRouter>,
+  document.getElementById("root"),
+);
