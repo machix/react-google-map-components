00c9adce813fe10ce02a7c4158baf9094eefc97b
Umidbek Karimov <uma.karimov@gmail.com>
2017-09-23 13:16:30 +0400

    test(FitBounds): add tests.

--- src/mocks/MapsMock.js
+++ src/mocks/MapsMock.js
@@ -10,14 +10,36 @@ export function createMapsMock() {
         this.listeners[event] = this.listeners[event] || [];
         this.listeners[event].push(listener);
       });
+
+      this.fitBounds = jest.fn();
+      this.PanBy = jest.fn();
+      this.PanTo = jest.fn();
+      this.PanToBounds = jest.fn();
     }),
 
-    Size: jest.fn(),
-    Point: jest.fn(),
-    LatLng: jest.fn(),
+    Size: jest.fn(function GoogleMapsSize(...args) {
+      this.args = args;
+    }),
+
+    Point: jest.fn(function GoogleMapsPoint(...args) {
+      this.args = args;
+    }),
+
+    LatLng: jest.fn(function GoogleMapsLatLng(latLng) {
+      this.latLng = latLng;
+      this.toJS = () => latLng;
+    }),
 
     LatLngBounds: jest.fn(function GoogleMapsLatLngBounds() {
-      this.extend = () => jest.fn();
+      this.extends = [];
+
+      this.extend = jest.fn(latLng => {
+        this.extends.push(latLng);
+
+        return this;
+      });
+
+      this.toJS = () => this.extends.map(x => (x.toJS ? x.toJS() : x));
     }),
 
     event: {
---
+++ src/modules/animations/__tests__/FitBounds.spec.js
@@ -0,0 +1,57 @@
+import React from "react";
+import { shallow } from "enzyme";
+import { FitBounds } from "../FitBounds";
+import { MapContext } from "../../internal/MapContext";
+import { createMapsMock } from "../../../mocks/MapsMock";
+
+describe("FitBounds", () => {
+  let mapContext;
+
+  beforeEach(() => {
+    const maps = createMapsMock();
+
+    mapContext = new MapContext(new maps.Map(), maps);
+  });
+
+  it("should fit bounds on mount", () => {
+    shallow(
+      <FitBounds
+        latLngBounds={[
+          { lat: 0, lng: 0 },
+          { lat: 1, lng: 1 },
+          { lat: 2, lng: 2 },
+          { lat: 3, lng: 3 },
+        ]}
+      />,
+      { context: { mapContext } },
+    );
+
+    expect(mapContext.map.fitBounds).toHaveBeenCalledTimes(1);
+    expect(mapContext.maps.LatLngBounds).toHaveBeenCalledTimes(1);
+    expect(mapContext.maps.LatLngBounds.mock.instances[0].toJS()).toEqual([
+      { lat: 0, lng: 0 },
+      { lat: 1, lng: 1 },
+      { lat: 2, lng: 2 },
+      { lat: 3, lng: 3 },
+    ]);
+  });
+
+  it("should fit bounds on mount", () => {
+    const wrapper = shallow(<FitBounds latLngBounds={[]} />, {
+      context: { mapContext },
+    });
+
+    expect(mapContext.map.fitBounds).toHaveBeenCalledTimes(1);
+    expect(mapContext.maps.LatLngBounds).toHaveBeenCalledTimes(1);
+    expect(mapContext.maps.LatLngBounds.mock.instances[0].toJS()).toEqual([]);
+
+    wrapper.setProps({ latLngBounds: [{ lat: 0, lng: 0 }] });
+
+    expect(mapContext.map.fitBounds).toHaveBeenCalledTimes(2);
+    expect(mapContext.maps.LatLngBounds).toHaveBeenCalledTimes(2);
+
+    expect(mapContext.maps.LatLngBounds.mock.instances[1].toJS()).toEqual([
+      { lat: 0, lng: 0 },
+    ]);
+  });
+});
