da2318fc936e425a623ba47c5da7a7bd2ac219f5
Umidbek Karimov <uma.karimov@gmail.com>
2017-10-05 21:50:12 +0400

    style(setupTests): format code.

--- src/setupTests.js
+++ src/setupTests.js
@@ -6,16 +6,32 @@ import { configure } from "enzyme";
 
 if (process.env.REACT_VERSION === "15") {
   jest.mock("react", () => require("react-15"));
-  jest.mock("react/lib/React", () => require("react-15/lib/React"), { virtual: true });
-  jest.mock("react/lib/ReactCurrentOwner", () => require("react-15/lib/ReactCurrentOwner"), { virtual: true });
-  jest.mock("react/lib/ReactComponentTreeHook", () => require("react-15/lib/ReactComponentTreeHook"), { virtual: true });
-  jest.mock("react/lib/getNextDebugID", () => require("react-15/lib/getNextDebugID"), { virtual: true });
+  jest.mock("react/lib/React", () => require("react-15/lib/React"), {
+    virtual: true,
+  });
+  jest.mock(
+    "react/lib/ReactCurrentOwner",
+    () => require("react-15/lib/ReactCurrentOwner"),
+    { virtual: true },
+  );
+  jest.mock(
+    "react/lib/ReactComponentTreeHook",
+    () => require("react-15/lib/ReactComponentTreeHook"),
+    { virtual: true },
+  );
+  jest.mock(
+    "react/lib/getNextDebugID",
+    () => require("react-15/lib/getNextDebugID"),
+    { virtual: true },
+  );
 
   jest.mock("react-dom", () => require("react-dom-15"));
   jest.mock("react-dom/server", () => require("react-dom-15/server"));
   jest.mock("react-dom/test-utils", () => require("react-dom-15/test-utils"));
   jest.mock("react-test-renderer", () => require("react-test-renderer-15"));
-  jest.mock("react-test-renderer/shallow", () => require("react-test-renderer-15/shallow"));
+  jest.mock("react-test-renderer/shallow", () =>
+    require("react-test-renderer-15/shallow"),
+  );
 
   const Adapter = require("enzyme-adapter-react-15");
 
